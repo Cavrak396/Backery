73a33f4fdc6754d3c5a3eeae7573c97e9c5cfa18
Cavrak396 <marko.kofs396@gmail.com>
2023-05-01 01:23:47 +0200

    update

--- dist/js/script.js
+++ dist/js/script.js
@@ -23,3 +23,37 @@ document.addEventListener("keydown", (event) => {
     document.querySelector("body").style.overflow = "scroll";
   }
 });
+
+const slider = document.querySelector(".js-about-slider");
+const slides = document.querySelectorAll(".js-slide");
+let slideHeight = 0;
+let currentSlideIndex = 0;
+
+function setSlideHeight() {
+  slideHeight = parseFloat(
+    window.getComputedStyle(slides[0]).getPropertyValue("height")
+  );
+}
+
+function moveSlide(direction) {
+  if (direction === "up") {
+    currentSlideIndex =
+      currentSlideIndex === 0 ? slides.length - 1 : currentSlideIndex - 1;
+  } else if (direction === "down") {
+    currentSlideIndex =
+      currentSlideIndex === slides.length - 1 ? 0 : currentSlideIndex + 1;
+  }
+
+  const slidePosition = currentSlideIndex * slideHeight * -1;
+  slider.style.transform = `translateY(${slidePosition}px)`;
+}
+
+window.addEventListener("resize", setSlideHeight);
+setSlideHeight();
+
+document
+  .querySelector(".js-btn-up")
+  .addEventListener("click", () => moveSlide("up"));
+document
+  .querySelector(".js-button-down")
+  .addEventListener("click", () => moveSlide("down"));
